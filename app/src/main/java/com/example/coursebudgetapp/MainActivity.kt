6c83eb0ffa832d375bfb6bf6749e7153709c5a69
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-05 16:37:25 +0100

    Test

--- app/src/main/java/com/example/coursebudgetapp/MainActivity.kt
+++ app/src/main/java/com/example/coursebudgetapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.coursebudgetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("LIFECYCLE", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "onDestroy")
     }
 }
