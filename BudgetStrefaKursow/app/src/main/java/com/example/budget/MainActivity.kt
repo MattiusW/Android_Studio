ded6971a63112476dac43f18a1700bc3045b7faa
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-07 01:20:35 +0100

    Long and short click

--- BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
@@ -3,18 +3,32 @@ package com.example.budget
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.budget.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.myButton.text = "Binding text change"
 
+        binding.myButton.setOnClickListener {
+            Toast.makeText(
+                applicationContext, "You'r click a button!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        binding.myButton.setOnLongClickListener {
+            Toast.makeText(applicationContext,
+                "Long click!",
+                Toast.LENGTH_SHORT).show()
+
+            true
+        }
 
     }
 }
