37f3720875b846807cbccec4a417c3d70380e279
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-07 12:16:41 +0100

    Bundle save data

--- BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
@@ -7,26 +7,40 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.net.toUri
 import com.example.budget.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var myValue = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.myButton.setOnClickListener {
-//            val intent = Intent(applicationContext, SecondActivity::class.java)
-            val intent = Intent(Intent.ACTION_DIAL)
-            val data = binding.myEditText.text.toString()
-            intent.data = "tel:$data".toUri()
-//            intent.putExtra("MY_DATA", data)
-            startActivity(intent)
+        if(savedInstanceState != null){
+            val mValue = savedInstanceState.getInt("VALUE")
+            myValue = mValue
+            binding.myTextView.text = myValue.toString()
         }
 
+        binding.myButton.setOnClickListener {
+            val rand = Random.nextInt(0, 100)
+            myValue = rand
+            binding.myTextView.text = myValue.toString()
+        }
 
+//        binding.myButton.setOnClickListener {
+////            val intent = Intent(applicationContext, SecondActivity::class.java)
+//            val intent = Intent(Intent.ACTION_DIAL)
+//            val data = binding.myEditText.text.toString()
+//            intent.data = "tel:$data".toUri()
+////            intent.putExtra("MY_DATA", data)
+//            startActivity(intent)
+//        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("VALUE", myValue)
     }
 }
