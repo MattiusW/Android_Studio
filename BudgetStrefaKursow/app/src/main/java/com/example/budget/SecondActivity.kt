53fa2ae59ca1c35e9bd344aa9ecc789421d8ff79
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-09 11:21:31 +0100

    Live Data

--- BudgetStrefaKursow/app/src/main/java/com/example/budget/SecondActivity.kt
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/SecondActivity.kt
@@ -4,32 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.budget.adapters.ContactsAdapter
 import com.example.budget.data.Contact
 import com.example.budget.databinding.ActivityMainBinding
 import com.example.budget.databinding.ActivitySecondBinding
+import com.example.budget.viewmodels.SecondViewModel
 
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private val vm by viewModels<SecondViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val c1 = Contact("John", "Smith", "532452311")
-        val c2 = Contact("Jenny", "Smith", "4545411")
-        val c3 = Contact("Jim", "Smith", "32412341")
-        val contactList = listOf(c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3)
-
         binding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
-        binding.recyclerView.adapter = ContactsAdapter(contactsList = contactList,
-            callListener = {
-                Toast.makeText(applicationContext, "Call to number: $it", Toast.LENGTH_SHORT).show()
-            },
-            smsListener = {
-                Toast.makeText(applicationContext, "Send sms to: $it", Toast.LENGTH_SHORT).show()
-            })
+
+        vm.contacts.observe(this) { contactList ->
+            binding.recyclerView.adapter = ContactsAdapter(contactsList = contactList,
+                callListener = {
+                    Toast.makeText(applicationContext, "Call to number: $it", Toast.LENGTH_SHORT).show()
+                },
+                smsListener = {
+                    Toast.makeText(applicationContext, "Send sms to: $it", Toast.LENGTH_SHORT).show()
+                })
+        }
+
+        vm.updateContactList()
+
+
     }
 }
---
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/viewmodels/SecondViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.budget.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.budget.data.Contact
+
+class SecondViewModel: ViewModel() {
+
+    private val _contacts = MutableLiveData<List<Contact>>()
+    val contacts: LiveData<List<Contact>> = _contacts
+    fun getContactList(): List<Contact> {
+        val c1 = Contact("John", "Smith", "532452311")
+        val c2 = Contact("Jenny", "Smith", "4545411")
+        val c3 = Contact("Jim", "Smith", "32412341")
+        return listOf(c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3)
+    }
+
+    fun updateContactList() {
+        val c1 = Contact("Nowy", "Gracz", "777")
+        _contacts.value = listOf(c1)
+    }
+}
