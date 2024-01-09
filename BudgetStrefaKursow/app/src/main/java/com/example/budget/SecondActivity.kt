d2fc4da96e226131df1c14cd43589c24f686aa2f
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-09 08:10:29 +0100

    Contact list

--- BudgetStrefaKursow/app/src/main/java/com/example/budget/SecondActivity.kt
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/SecondActivity.kt
@@ -2,14 +2,34 @@ package com.example.budget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.budget.adapters.ContactsAdapter
+import com.example.budget.data.Contact
+import com.example.budget.databinding.ActivityMainBinding
+import com.example.budget.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val data = intent.getStringExtra("MY_DATA")
-        Toast.makeText(applicationContext, data, Toast.LENGTH_SHORT).show()
+        val c1 = Contact("John", "Smith", "532452311")
+        val c2 = Contact("Jenny", "Smith", "4545411")
+        val c3 = Contact("Jim", "Smith", "32412341")
+        val contactList = listOf(c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3,c1,c2,c3)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        binding.recyclerView.adapter = ContactsAdapter(contactsList = contactList,
+            callListener = {
+                Toast.makeText(applicationContext, "Call to number: $it", Toast.LENGTH_SHORT).show()
+            },
+            smsListener = {
+                Toast.makeText(applicationContext, "Send sms to: $it", Toast.LENGTH_SHORT).show()
+            })
     }
 }
---
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/data/Contact.kt
@@ -0,0 +1,3 @@
+package com.example.budget.data
+
+data class Contact(val name: String, val surname: String, val number: String)
