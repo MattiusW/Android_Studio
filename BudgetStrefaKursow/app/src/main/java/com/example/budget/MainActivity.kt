3348ab883c05f9ab9d703652cfa843beabe88aef
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-25 16:17:53 +0100

    DataStore

--- BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
+++ BudgetStrefaKursow/app/src/main/java/com/example/budget/MainActivity.kt
@@ -3,24 +3,45 @@ package com.example.budget
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.net.toUri
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.NavHostFragment
 import com.example.budget.databinding.ActivityMainBinding
 import com.example.budget.fragments.FirstFragment
 import com.example.budget.fragments.SecondFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val intKey = intPreferencesKey("myInt")
 //    private var myValue = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStore.edit {
+                it[intKey] = 777
+            }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStore.edit {prefs ->
+                val myPrefs = prefs[intKey]
+                Log.d("PREFS", "MyPrefs:$myPrefs")
+            }
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(binding.fragContainerFirst.id) as NavHostFragment
         val navController = navHostFragment.navController
