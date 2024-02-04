ad69ba7181393e806419d2395dedd3ca13c15d1b
Mateusz Walczyk <mattiusw@gmail.com>
2024-02-04 12:26:21 +0100

    Make SQL Query

--- MyBudget/app/src/main/java/com/mw/mybudget/data/room/TransactionDao.kt
+++ MyBudget/app/src/main/java/com/mw/mybudget/data/room/TransactionDao.kt
@@ -3,8 +3,10 @@ package com.mw.mybudget.data.room
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.mw.mybudget.data.model.Transaction
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TransactionDao {
@@ -17,4 +19,16 @@ interface TransactionDao {
 
     @Delete
     fun deleteTransactions(transactions: List<Transaction>)
+
+    @Query("SELECT * FROM transactions_table ORDER BY date DESC")
+    fun getAllTransactions(): Flow<List<Transaction>>
+
+    @Query("SELECT * FROM transactions_table WHERE type = 'INCOME' ")
+    fun getAllIncomes(): Flow<List<Transaction>>
+
+    @Query("SELECT * FROM transactions_table WHERE type = 'OUTCOME' ")
+    fun getAllOutcomes(): Flow<List<Transaction>>
+
+    @Query("SELECT category, SUM(price) as total FROM transactions_table WHERE type = 'INCOME' GROUP BY category")
+    fun getSumOfIncomesGroupByCategory()
 }
