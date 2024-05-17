2986f0470a373bf188c7efafd2ed02473fb82c0d
Mateusz Walczyk <mattiusw@gmail.com>
2024-05-17 10:49:20 +0200

    Add empty activity

--- DziennikTransakcji/app/build.gradle.kts
+++ DziennikTransakcji/app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
