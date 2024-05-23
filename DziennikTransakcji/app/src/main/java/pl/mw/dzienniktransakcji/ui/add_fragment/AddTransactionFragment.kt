8834fb9dfe4b86ecff37a36bab169131c27fe26f
Mateusz Walczyk <mattiusw@gmail.com>
2024-05-23 18:13:21 +0200

    Made logic to add transaction and create calendar

--- DziennikTransakcji/app/src/main/java/pl/mw/dzienniktransakcji/ui/add_fragment/AddTransactionFragment.kt
+++ DziennikTransakcji/app/src/main/java/pl/mw/dzienniktransakcji/ui/add_fragment/AddTransactionFragment.kt
@@ -6,20 +6,88 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.activityViewModels
+import pl.mw.dzienniktransakcji.MainViewModel
 import pl.mw.dzienniktransakcji.R
+import pl.mw.dzienniktransakcji.data.room.Transaction
+import pl.mw.dzienniktransakcji.data.room.TransactionCategory
+import pl.mw.dzienniktransakcji.data.room.TransactionType
+import pl.mw.dzienniktransakcji.databinding.FragmentAddTransactionBinding
+import pl.mw.dzienniktransakcji.ui.date_picker.TransactionDatePicker
+import java.util.Calendar
 
 class AddTransactionFragment : Fragment() {
 
     private val viewModel by viewModels<AddTransactionViewModel>()
+    private val mainVm by activityViewModels<MainViewModel>()
+    private var _binding: FragmentAddTransactionBinding? = null
+    private  val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+    ): View? {
+        _binding = FragmentAddTransactionBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter(requireContext(),
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            TransactionCategory.values().map { enum -> enum.name })
+
+        binding.categorySpinner.adapter = adapter
+
+        binding.calendarImage.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        binding.saveBtn.setOnClickListener {
+            val trans = createTransaction()
+            mainVm.insertTransaction(trans)
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val newDatePicker = TransactionDatePicker {day, month, year ->
+            binding.dayTv.text = day.toString()
+            binding.monthTv.text = month.toString()
+            binding.yearTv.text = year.toString()
+
+            val date = Calendar.getInstance()
+            date.set(day, month, year)
+            viewModel.date = date.timeInMillis
+        }
+
+        newDatePicker.show(parentFragmentManager, "DatePicker")
+    }
+
+    private fun createTransaction(): Transaction {
+        val type = when(binding.typeRg.checkedRadioButtonId) {
+            binding.incomeRb.id -> TransactionType.INCOME
+            else -> TransactionType.OUTCOME
+        }
+
+        val category = when(binding.categorySpinner.selectedItem.toString()) {
+            "FOOD" -> TransactionCategory.FOOD
+            "OTHERS" -> TransactionCategory.OTHERS
+            "HOUSEHOLD" -> TransactionCategory.HOUSEHOLD
+            "TRANSPORTATION" -> TransactionCategory.TRANSPORTATION
+            else -> TransactionCategory.OTHERS
+        }
+
+        val amount = binding.amountEt.text.toString()
+        val desc = binding.descEt.text.toString()
+
+        return Transaction(0, viewModel.date, amount.toFloat(), desc, type, category)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- DziennikTransakcji/app/src/main/java/pl/mw/dzienniktransakcji/ui/add_fragment/AddTransactionViewModel.kt
+++ DziennikTransakcji/app/src/main/java/pl/mw/dzienniktransakcji/ui/add_fragment/AddTransactionViewModel.kt
@@ -3,5 +3,5 @@ package pl.mw.dzienniktransakcji.ui.add_fragment
 import androidx.lifecycle.ViewModel
 
 class AddTransactionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var date = -1L
 }
